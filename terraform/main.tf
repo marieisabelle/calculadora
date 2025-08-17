5907465ef3711c91a1b28092d8ce94acec29520e
Isabelle Marie <isabellemarie82@gmail.com>
2025-08-16 23:48:57 -0400

    fix main

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,6 @@ variable "bucket_name" {
   default = "calculadora-terraform-state"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
-}
-
 resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
