6b93cd61cfc3d51146a4cb5514121c9ff2a062cb
Isabelle Marie <isabellemarie82@gmail.com>
2025-08-17 09:13:54 -0400

    destruicao do s3 em 35 min apos o funcionamento

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,19 @@ variable "bucket_name" {
   default = "calculadora-terraform-state"
 }
 
+data "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+}
+
 resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = data.aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = data.aws_s3_bucket.terraform_state.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -21,29 +25,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
+  bucket = data.aws_s3_bucket.terraform_state.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t3.micro" 
-
-  tags = {
-    Name = "Calculadora-Instance"
-  }
 }
