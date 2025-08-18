f9ee73e129e5fd0aeef25604203b761b3f3e3909
Isabelle Marie <isabellemarie82@gmail.com>
2025-08-18 14:52:31 -0400

    ajuste no nome correto do repositorio

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 set -e
 
 echo "Parando container antigo"
-docker stop minha-app || true
-docker rm minha-app || true
+docker stop calculadora || true
+docker rm calculadora || true
 
 echo "Baixando imagem do DockerHub"
-docker pull $DOCKER_USERNAME/minha-app:latest
+docker pull $DOCKER_USERNAME/calculadora:latest
 
 echo "Subindo novo container"
-docker run -d --name minha-app -p 80:80 $DOCKER_USERNAME/minha-app:latest
+docker run -d --name calculadora -p 80:80 $DOCKER_USERNAME/calculadora:latest
